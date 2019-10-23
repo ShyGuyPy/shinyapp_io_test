ccdd8233698cb4504aea98cf47e4e79192abc25e
ShyGuyPy <luke.vawter1@gmail.com>
2019-10-23 11:38:19 -0400

    commmit before attempt at final fix

--- global.R
+++ global.R
@@ -7,3 +7,11 @@ source("config/paths.R", local = TRUE)
 source("input/parameters/parameters.R", local = TRUE)
 source("code/global/import_data.R", local = TRUE)#
 #----------------------------------------------------------------------------
+
+
+source("code/functions/display/md_drought_map_func.R", local = TRUE)
+source("code/functions/display/va_drought_map_func.R", local = TRUE)
+
+deploy_test <- curl::curl_download('https://deq1.bse.vt.edu/drought/state/images/maps/')
+
+open(deploy_test)
