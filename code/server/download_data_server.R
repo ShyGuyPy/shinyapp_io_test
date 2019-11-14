922f262faeee7934abf357e759bd5b4440b62178
ShyGuyPy <luke.vawter1@gmail.com>
2019-11-14 14:49:12 -0500

    download and plto of all three data sets/types are functional

--- code/server/download_data_server.R
+++ code/server/download_data_server.R
@@ -1,4 +1,8 @@
-# download data from withdrawal_data
+###-----------------------tab for downloading withdrawal and flows data from Sarah's Drupal site------------------------------------
+
+
+#---------------------------------------download data from withdrawal_data----------------------------------------------------------
+
 observeEvent(input$download_data_w, {
   
   # gages <- data.table::fread(paste(parameters_path, "gages.csv", sep = ""),
@@ -7,9 +11,15 @@ observeEvent(input$download_data_w, {
   # list_gage_locations <- c("date", gages$location)
   
   #import the data from sarah's site
-  withProgress(message = "downloading data", value = 0, {
+  withProgress(message = "downloading withdawals data", value = 0, {
+    #increment progress bar
+    incProgress(1/2)
   demands_raw.df <- data.table::fread("http://icprbcoop.org/drupal4/products/coop_pot_withdrawals.csv",
                                       header = TRUE, data.table = FALSE) #, colClasses = list_gage_locations)
+  #increment progress bar
+  incProgress(1/2)
+  
+
 })
   
   #gather the data into long format
@@ -47,6 +57,7 @@ observeEvent(input$accept_data_w, {
   write.csv(demands.df, paste(ts_path, "coop_pot_withdrawals.csv"))
 })
 
+#------------------------------------------------------------------------------------------------------------------------------------
 
 
 
@@ -66,10 +77,13 @@ observeEvent(input$accept_data_w, {
 
 
 
-
-#download data from flows daily
+#----------------------------------------------download data from flows daily------------------------------------------------------
 
 observeEvent(input$download_data_fd, {
+  #creates a progess bar for data download
+  withProgress(message = "downloading flows daily data", value = 0, {
+    #increment progress bar
+    incProgress(1/2)
   
   #construct path for daily flows data
   first_of_year = as.Date("2019-1-1")
@@ -81,18 +95,10 @@ observeEvent(input$download_data_fd, {
   
   full_daily_url = paste0("https://icprbcoop.org/drupal4/icprb/flow-data?","startdate=", daily_start,"&enddate=" ,daily_end, "&format=daily&submit=Submit")
   
-  print(full_daily_url)
-  
-  #-----------------------------
-  
-  gages <- data.table::fread(paste(parameters_path, "gages.csv", sep = ""),
-                             col.names = c("id", "location", "description"),
-                             data.table = FALSE)
-  list_gage_locations <- c("date", gages$location)
-  
   #import the data from sarah's site
   demands_raw.df <- data.table::fread(full_daily_url, header = TRUE,
-                                      data.table = FALSE, colClasses = list_gage_locations)
+                                      data.table = FALSE,   colClasses = c("character", rep("numeric", 31)), # force cols 2-32 numeric
+                                      col.names = list_gage_locations,)
   
   #gather the data into long format
   demands.df <- gather(demands_raw.df,site, flow, 2:32)
@@ -102,6 +108,11 @@ observeEvent(input$download_data_fd, {
   
   #write dataframe to file
   write.csv(demands.df, paste(ts_path, "download_data_fd_temp.csv"))
+  
+  #increment progress bar
+  incProgress(1/2)
+  
+  }) #end of withProgress
 })
 
 observeEvent(input$view_data_fd, {
@@ -130,6 +141,7 @@ observeEvent(input$accept_data_fd, {
   write.csv(demands.df, paste(ts_path, "flows_daily_cfs.csv"))#"download_data_fd.csv"))
 })
 
+#---------------------------------------------------------------------------------------------------------------------------
 
 
 
@@ -147,12 +159,15 @@ observeEvent(input$accept_data_fd, {
 
 
 
-
-#download data flows hourly
+#---------------------------------------------------download data flows hourly---------------------------------------------
 
 observeEvent(input$download_data_fh, {
-  #construct file path
+  #creates a progess bar for data download
+  withProgress(message = "downloading flows daily data", value = 0, {
+    #increment progress bar
+    incProgress(1/2)
   
+  #construct file path
   date_minus_five = date_today0 - 5
   hourly_start = paste0(format(date_minus_five,"%m"),"%2F",format(date_minus_five,"%d"), "%2F",format(date_minus_five,"20%y"))
   
@@ -162,7 +177,8 @@ observeEvent(input$download_data_fh, {
   
   #import the hourly flows data from sarah's site
   demands_raw.df <- data.table::fread(full_hourly_url, header = TRUE,
-                                      data.table = FALSE)
+                                      data.table = FALSE, colClasses = c("character", rep("numeric", 31)), # force cols 2-32 numeric
+                                      col.names = list_gage_locations,)
   
   #gather the data into long format
   demands.df <- gather(demands_raw.df,site, flow, 2:32)
@@ -172,6 +188,10 @@ observeEvent(input$download_data_fh, {
   
   #write dataframe to file
   write.csv(demands.df, paste(ts_path, "download_data_fh_temp.csv"))
+  
+  #increment progress bar
+  incProgress(1/2)
+  }) #end of withProgress
 })
 
 observeEvent(input$view_data_fh, {
@@ -205,3 +225,4 @@ observeEvent(input$accept_data_fh, {
   write.csv(demands.df, paste(ts_path, "flows_hourly_cfs.csv"))#"download_data_fh.csv"))
 })
 
+#--------------------------------------------------------------------------------------------------------------------------------
